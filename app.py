691f411af7ce725436267772d8f2ac9710db23cd
Tamar <talphaidze.ieu2020>
2022-11-03 10:24:39 +0100

    flask exercise

--- app.py
+++ app.py
@@ -46,9 +46,20 @@ def delete(id):
     
     
 
-@app.route('/update')
-def update():
-    return render_template('update.html')
+@app.route('/update/<int:id>',  methods=['POST', 'GET'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+    
+    if request.method == 'POST':
+        task.content = request.form['update_content']
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was a problem updating that task'
+    else:
+        return render_template('update.html', task=task)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
